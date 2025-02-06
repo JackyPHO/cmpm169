f6200f7875319cc30317cbca32c33f694ef4ddc5
JackyPHO <100171161+JackyPHO@users.noreply.github.com>
2025-02-05 17:49:10 -0800

    ex4v1

---
+++ experiment4/js/sketch.js
@@ -0,0 +1,55 @@
+// sketch.js - purpose and description here
+// Author: Jacky Ho
+// Date: February 4, 2025
+
+// Here is how you might set up an OOP p5.js project
+// Note that p5.js looks for a file called sketch.js
+
+// Constants - User-servicable parts
+// In a longer project I like to put these in a separate file
+
+// Globals
+
+class MyClass {
+    constructor(param1, param2) {
+        this.property1 = param1;
+        this.property2 = param2;
+    }
+
+    myMethod() {
+        // code to run when method is called
+    }
+}
+
+function resizeScreen() {
+  centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
+  centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
+  console.log("Resizing...");
+  resizeCanvas(canvasContainer.width(), canvasContainer.height());
+  // redrawCanvas(); // Redraw everything based on new size
+}
+
+// setup() function is called once when the program starts
+function setup() {
+  // place our canvas, making it fit our container
+  canvasContainer = $("#canvas-container");
+  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
+  canvas.parent("canvas-container");
+  // resize canvas is the page is resized
+
+  // create an instance of the class
+  myInstance = new MyClass("VALUE1", "VALUE2");
+
+  $(window).resize(function() {
+    resizeScreen();
+  });
+  resizeScreen();
+}
+
+// draw() function is called repeatedly, it's the main animation loop
+function draw() {
+}
+
+//Integrate: Change mouse position https://p5js.org/reference/p5/mouseClicked/
+function mouseClicked(){
+}
